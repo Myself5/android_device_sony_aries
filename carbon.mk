1162fb0ac2dbcf1ba8f460b64b0d52865463da17
Myself5 <myself5@carbonrom.org>
2015-02-15 15:38:16 -0500

    Carbonify and add MultiROM support

--- carbon.mk
+++ carbon.mk
@@ -1,8 +1,5 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit Carbon common Phone stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 $(call inherit-product, device/sony/z3c/full_z3c.mk)
 
@@ -10,5 +7,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5803
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D5803/D5803:4.4.4/23.0.A.2.105/lvf_Rw:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D5803-user 4.4.4 23.0.A.2.105 lvf_Rw release-keys"
 
-PRODUCT_NAME := cm_z3c
+PRODUCT_NAME := carbon_z3c
 PRODUCT_DEVICE := z3c
